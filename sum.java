a735d84ae5ac2ae6eb5d3584654f31fccceb3b8e
Prateek <prateekverma393@gmail.com>
2023-03-06 10:41:56 +0530

    new copde written

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+    Run|Debug
+    public static void main(String[] args){
+        int a = 4;
+    int b = 8;
+
+    System.out.println("This is a sum"+ (a+b));
+    }
+
+    
+
+
+}
